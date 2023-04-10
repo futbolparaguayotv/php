15664e71701282614e77ec98e265d5faf9047aea
futbolparaguayotv <56748248+futbolparaguayotv@users.noreply.github.com>
2023-04-10 16:15:44 -0300

    Create agenda.php

---
+++ agenda.php
@@ -0,0 +1,196 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<script type="text/javascript" src="https://www.famustv.com/hora.js"></script>
+<meta http-equiv="cache-control" content="no-cache" />
+<meta http-equiv="Pragma" content="no-cache" />
+<meta http-equiv="expires" content="-1" />
+<meta name="googlebot" content="noindex">
+<title>Agenda Deportiva - FUTBOL </title>
+<link rel="stylesheet" type="text/css" href="https://www.famustv.com/famus.css">
+<style type="text/css"<style type="text/css">
+.menu>li>a:hover,.menu>li>a.active{background-color:#fa0000;background-image:-webkit-gradient(linear,left top,left bottom,from(rgb(61, 177, 227)),to(rgb(61, 177, 227)))
+</style>>
+</style>
+ <!--Bloquear Click Derecgo-->
+<script language="JavaScript">
+  function disableIE() {
+      if (document.all) { alert(msg); return false; }
+  }
+  function disableNS(e) {
+      if (document.layers || (document.getElementById && !document.all)) {
+          if (e.which == 2 || e.which == 3) { alert(msg); return false; }
+      }
+  }
+  if (document.layers) {
+      document.captureEvents(Event.MOUSEDOWN); document.onmousedown = disableNS;
+  } else {
+      document.onmouseup = disableNS; document.oncontextmenu = disableIE;
+  }
+  document.oncontextmenu = ev =>{
+    ev.preventDefault();
+    console.log("Prevented to open menu!");
+  }
+</script>
+<!--Fin Bloquear Click Derecgo--> 
+<script>
+    // Verificar si el script ya se ha ejecutado hoy
+    var today = new Date().toLocaleDateString();
+    var lastExecuted = localStorage.getItem('lastExecuted');
+    if (lastExecuted === today) {
+      // El script ya se ejecutó hoy, no hacer nada
+      console.log('El script ya se ejecutó hoy.');
+    } else {
+      // El script aún no se ha ejecutado hoy, ejecutarlo
+      var script = document.createElement('script');
+      script.setAttribute('data-adel', 'lwsu');
+      script.setAttribute('zid', '6686846');
+      document.head.appendChild(script);
+  
+      // Guardar la fecha de la última ejecución en localStorage
+      localStorage.setItem('lastExecuted', today);
+      console.log('El script se ejecutó correctamente.');
+    }
+  </script>-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+</head>
+<body link='#547484' onload='guardaHorario();dT();' vlink='#547484'>
+<div id="wraper">
+<ul class="menu">
+<center><div class="sombreada_css3"><font size=3 face="Helvetica Neue,Helvetica,Arial,sans-serif" color="#FFFFFF">
+<b>AGENDA DEPORTIVA FAMUSTV 10 DE ABRIL 2023</b>
+</font></div></center>
+<li class="URU"><a href="#">
+Primera División: Liverpool vs. Plaza Colonia
+<span class="t">20:30</span></a>
+<ul>
+<li class="subitem1"><a href="https://www.famustv.com/go/vtv+.html" target="_blank">VTV +<span>Calidad 720p</span></a></li>
+<li class="subitem1"><a href="https://www.famustv.com/go/star++.html" target="_blank">Star+<span>Calidad 720p</span></a></li>
+</ul>
+</li>
+<li class="ES"><a href="#">
+LaLiga: Barcelona vs. Girona
+<span class="t">21:00</span></a>
+<ul>
+<li class="subitem1"><a href="https://www.famustv.com/go/dsports.html" target="_blank">DSports<span>Calidad 720p</span></a></li>
+<li class="subitem1"><a href="https://www.famustv.com/go/daznlaliga.html" target="_blank">DAZN La Liga<span>Calidad 720p</span></a></li>
+<li class="subitem1"><a href="https://famustv.com/embed/eventoss.html?r=aHR0cHM6Ly9mYW11c3R2LmNvbS9jYW5hbGVzL0VsZXZlbnNwb3J0czEucGhw" target="_blank">Eleven Sports 1 PL<span>Calidad 720p</span></a></li>
+<li class="subitem1"><a href="https://famustv.com/embed/eventoss.html?r=aHR0cHM6Ly9mYW11c3R2LmNvbS9jYW5hbGVzL0VzcG5EZXBvcnRlc1VzLnBocA==" target="_blank">ESPN Deportes US<span>Calidad 720p</span></a></li>
+<li class="subitem1"><a href="https://famustv.com/embed/eventoss.html?r=aHR0cHM6Ly9mYW11c3R2LmNvbS9DbW9yZTFzcG9ydHNmbC5waHA=" target="_blank">C More Sport 1 FI<span>Calidad 720p</span></a></li></li>
+
+</ul>
+  </li>
+<li class="POR"><a href="#">
+Primeira Liga: Gil Vicente vs. Chaves
+ <span class="t">21:15</span></a>
+<ul>
+<li class="subitem1"><a href="https://famustv.com/embed/eventoss.html?r=aHR0cHM6Ly9mYW11c3R2LmNvbS9wb3J0dWdhbGV2ZW50by5waHA=" target="_blank">SportTV 1 PT (Ads)<span>Calidad 720p</span></a></li>
+</ul>
+</li>
+<li class="PE"><a href="#">
+Liga 1: AD Cantolao vs. Unión Comercio
+<span class="t">22:30</span></a>
+<ul>
+<li class="subitem1"><a href="https://www.famustv.com/go/ligatvmax.html" target="_blank">Liga 1 MAX PE<span>Calidad 720p</span></a></li>
+</ul>
+</li>
+<li class="COL"><a href="#">
+Liga BetPlay: Atlético Nacional vs. Junior
+<span class="t">01:00</span></a>
+<ul>
+<li class="subitem1"><a href="https://www.famustv.com/go/win+.html" target="_blank">Win Sports +<span>Calidad 720p</span></a></li>
+<li class="subitem1"><a href="https://famustv.com/go/VIX+.html" target="_blank">VIX+<span>Calidad 1080p</span></a></li>
+</ul>
+</li>
+<li class="PE"><a href="#">
+Liga 1: César Vallejo vs. Deportivo Garcilaso
+<span class="t">01:00</span></a>
+<ul>
+<li class="subitem1"><a href="https://www.famustv.com/go/ligatvmax.html" target="_blank">Liga 1 MAX PE<span>Calidad 720p</span></a></li>
+</ul>
+</li>
+<li class="URU"><a href="#">
+Primera División: La Luz vs. Boston River
+<span class="t">01:15</span></a>
+<ul>
+<li class="subitem1"><a href="https://www.famustv.com/go/vtv+.html" target="_blank">VTV +<span>Calidad 720p</span></a></li>
+<li class="subitem1"><a href="https://www.famustv.com/go/star++.html" target="_blank">Star+<span>Calidad 720p</span></a></li>
+</ul>
+</li>
+<li class="MLB"><a href="#">
+MLB: Reds vs. Braves
+<span class="t">01:30</span></a>
+<ul>
+<li class="subitem1"><a href="https://www.famustv.com/go/star++.html" target="_blank">Star+<span>Calidad 720p</span></a></li>
+</ul>
+</li>
+<li class="WWE"><a href="#">
+WWE: RAW
+<span class="t">02:00</span></a>
+<ul>
+<li class="subitem1"><a href="https://www.famustv.com/go/star++.html" target="_blank">Star+<span>Calidad 720p</span></a></li>
+</ul>
+</li>
+<li class="ECUA"><a href="#">
+Liga Pro: Barcelona vs. Cumbayá
+<span class="t">02:00</span></a>
+<ul>
+<li class="subitem1"><a href="https://www.famustv.com/go/goltv.html" target="_blank">GolTV<span>Calidad 720p</span></a></li>
+<li class="subitem1"><a href="https://www.famustv.com/go/star++.html" target="_blank">Star+<span>Calidad 720p</span></a></li>
+</ul>
+</li>
+<li class="COL"><a href="#">
+Liga BetPlay: Deportes Tolima vs. Deportivo Pasto
+<span class="t">03:05</span></a>
+<ul>
+<li class="subitem1"><a href="https://www.famustv.com/go/win+.html" target="_blank">Win Sports +<span>Calidad 720p</span></a></li>
+<li class="subitem1"><a href="https://famustv.com/go/VIX+.html" target="_blank">VIX+<span>Calidad 1080p</span></a></li>
+</ul>
+</li>
+</li>
+</div>
+<!--initiate accordion-->
+<script type="text/javascript">
+	$(function() {
+	
+	 var menu_ul = $('.menu > li > ul'),
+	 menu_a = $('.menu > li > a');
+	 
+	 menu_ul.hide();
+	
+	 menu_a.click(function(e) {
+	 e.preventDefault();
+	 if(!$(this).hasClass('active')) {
+	 menu_a.removeClass('active');
+	 menu_ul.filter(':visible').slideUp('fast');
+	 $(this).addClass('active').next().stop(true,true).slideDown('fast');
+	 } else {
+	 $(this).removeClass('active');
+	 $(this).next().stop(true,true).slideUp('fast');
+	 }
+	 });
+	
+	});
+	
+	
+
+</script>
+
+<br>
+
+</div>
+<script>
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+
+</script>
+</body>
+</html>
